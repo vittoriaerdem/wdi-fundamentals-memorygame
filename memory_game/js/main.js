36a8390997dcd505f762872a716c9d8541265a79
Vittoria Erdem <vittoria@Vittorias-MacBook-Pro.local>
2019-07-16 21:41:08 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,18 @@
-console.log("Up and running!");
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "queen";
-var cardFour = "queen";
-
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardTwo);
+
+
+
+if (cardsInPlay[0] === cardsInPlay[2]) {
+ 	alert("You found a match!");
+ } else {
+ 	alert("Sorry, try again.");
+}
