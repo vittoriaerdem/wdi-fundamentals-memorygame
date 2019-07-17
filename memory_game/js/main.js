3678c468f2114f083417946257cd37aa8c965365
Vittoria Erdem <vittoria@Vittorias-MacBook-Pro.local>
2019-07-16 20:57:41 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "queen";
+var cardFour = "queen";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
